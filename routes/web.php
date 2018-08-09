c6a9337d5a7a9c120bde2325efa79165bb579bf1
newtonkim <newton@ensibuuko.com>
2018-08-09 09:30:11 +0000

    Authentication feature

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,6 @@
 use Illuminate\Http\Request;
 
 Route::resource('links', 'LinkController');
+//authentication
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-// Route::post('/submit', function (Request $request) {
-//     $data = $request->validate([
-//         'title' => 'required|max:255',
-//         'url' => 'required|url|max:255',
-//         'description' => 'required|max:255',
-//     ]);
-
-//     $link = tap(new App\Link($data))->save();
-
-//     return redirect('/');
-// });
